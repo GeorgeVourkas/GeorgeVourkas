e59132bfed62feb061a3b39d51ddabbacef18847
George Vourkas <158522369+GeorgeVourkas@users.noreply.github.com>
2024-03-12 12:23:39 +0200

    Add files via upload

--- animations.js
+++ animations.js
@@ -2,26 +2,16 @@ const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry) => {
         if(entry.isIntersecting){
             entry.target.classList.add("show");
-        } //else{
-            //entry.target.classList.remove("show");
-        //}
-    });
-});
-
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((e1) => observer.observe(e1));
-
-const observer2 = new IntersectionObserver((entries)=>{
-    entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            entry.target.classList.add("show2");
         } 
     });
 });
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((e1) => observer.observe(e1)); 
 
-const hiddenElements2 = document.querySelectorAll(".hidden2");
-
-hiddenElements2.forEach((e1) => observer2.observe(e1)); // Corrected line: observe with observer2
-
-// Disable scrolling via JavaScript (optional)
+// Disable scrolling 
 document.body.style.overflow = "hidden";
+
+//Disable Right Click
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
